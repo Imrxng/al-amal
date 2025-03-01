076e39b2e7a47ceb79d2e4ccec1e08b002f3f505
imrxng <imgha@hotmail.com>
2025-03-01 13:02:25 +0100

    m

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -6,13 +6,13 @@ import ReactDOMServer from 'react-dom/server';
 
 const Map = () => {
   const CUSTOM_ICON = new L.DivIcon({
-    html: ReactDOMServer.renderToString(<FaMapMarkerAlt style={{ color: '#ED1C24', fontSize: '35px' }}/>),
+    html: ReactDOMServer.renderToString(<FaMapMarkerAlt style={{ color: 'var(--secondary-font)', fontSize: '35px' }}/>),
     className: "custom-icon",
     iconSize: [35, 35]
   });
 
   return (
-    <MapContainer center={[51.2206093675261, 4.4592743]} zoom={14} scrollWheelZoom={false} style={{ height: "500px", backgroundColor: '#f9f9f9', marginTop: '20rem',borderRadius: '10px'}}>
+    <MapContainer center={[51.2206093675261, 4.4592743]} zoom={14} scrollWheelZoom={false} style={{ height: "100%", width:'60%', backgroundColor: '#f9f9f9',borderRadius: '10px'}}>
       <TileLayer
         attribution="&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
         url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
