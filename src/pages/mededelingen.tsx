ef466696e85480558ae406a8ea363d0502c0f7e4
imrxng <imgha@hotmail.com>
2025-06-11 16:24:20 +0200

    added new routes

---
+++ src/pages/mededelingen.tsx
@@ -0,0 +1,15 @@
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import React from 'react'
+
+const Mededelingen = () => {
+  return (
+    <>
+        <Header />
+
+        <Footer />
+    </>
+  )
+}
+
+export default Mededelingen
